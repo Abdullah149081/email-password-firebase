e41d24c2c405cf138d1ee9e05c7b88ec1ee46340
Abdullah149081 <masudmd425@gmail.com>
2023-04-19 16:05:58 +0600

    event.preventDefault

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Register = () => {
+  const handlerSubmit = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+  };
+
   return (
     <div className="hero mt-20">
       <div className="hero-content flex-col w-full  lg:w-5/6 ">
@@ -9,25 +15,25 @@ const Register = () => {
           <h1 className="text-2xl lg:text-5xl font-bold">Please Register!</h1>
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <form className="card-body">
+          <form onSubmit={handlerSubmit} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Name</span>
               </label>
-              <input type="text" name="name" placeholder="Type your name" className="input input-bordered" />
+              <input type="text" name="name" placeholder="Type your name" className="input input-bordered" required />
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
-              <input type="email" name="email" placeholder="email" className="input input-bordered" />
+              <input type="email" name="email" placeholder="email" className="input input-bordered" required />
             </div>
 
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
-              <input type="password" name="password" placeholder="password" className="input input-bordered" />
+              <input type="password" name="password" placeholder="password" className="input input-bordered" required />
               <label className="mt-4 text-lg ">
                 Already account?
                 <span>
