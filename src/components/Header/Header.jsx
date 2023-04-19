71ed4e58c795528b4a9d0d1e05766a16a55c1a6d
Abdullah149081 <masudmd425@gmail.com>
2023-04-19 15:41:25 +0600

    register page done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import ActiveLink from "../ActiveLink/ActiveLink";
 
 const Header = () => {
   return (
-    <nav className="flex justify-center gap-6 mt-8">
-      <Link className="btn" to="/">
+    <nav className="flex justify-center  w-1/3 mx-auto  flex-col lg:flex-row   text-center   gap-6 mt-8">
+      <ActiveLink className="default" to="/">
         Home
-      </Link>
-      <Link className="btn" to="/sign-in">
+      </ActiveLink>
+      <ActiveLink className="default" to="/sign-in">
         Sign in
-      </Link>
-      <Link className="btn" to="/sign-up">
+      </ActiveLink>
+      <ActiveLink className="default" to="/sign-up">
         Sign up
-      </Link>
+      </ActiveLink>
     </nav>
   );
 };
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React from "react";
 
 const Login = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="hero mt-20">
+      <div className="hero-content flex-col w-full  lg:w-5/6 ">
+        <div className="text-center ">
+          <h1 className="text-2xl lg:text-5xl font-bold">Please Register!</h1>
         </div>
-    );
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input type="text" name="name" placeholder="Type your name" className="input input-bordered" />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input type="email" name="email" placeholder="email" className="input input-bordered" />
+            </div>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input type="password" name="password" placeholder="password" className="input input-bordered" />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
