bf3fa882c111a5b66688d5f9777df17b8626038b
Abdullah149081 <masudmd425@gmail.com>
2023-04-20 20:44:27 +0600

    login firebase add

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import app from "../../Firebase/firebase.config";
 
+const auth = getAuth(app);
 const Login = () => {
+  const [error, setError] = useState("");
+
+  const handlerLogin = (e) => {
+    setError("");
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {})
+      .catch((err) => {
+        setError(err.message);
+      });
+  };
+
   return (
     <div className="hero mt-20">
       <div className="hero-content flex-col w-full  lg:w-5/6 ">
@@ -9,7 +27,7 @@ const Login = () => {
           <h1 className="text-2xl lg:text-5xl font-bold">Please Login!</h1>
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <form className="card-body">
+          <form onSubmit={handlerLogin} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
@@ -36,6 +54,7 @@ const Login = () => {
                 </span>
               </label>
             </div>
+            <p className="text-red-600 font-medium">{error}</p>
             <div className="form-control mt-6">
               <button className="btn btn-primary">Login</button>
             </div>
